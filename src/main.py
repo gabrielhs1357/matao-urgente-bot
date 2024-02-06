986fafd2a206a7f9a2a8ba1adcd78a942a3046e9
Gabriel Silva <gabriel.henrique@quintoandar.com.br>
2024-02-05 22:17:26 -0300

    add some chrome options

--- src/main.py
+++ src/main.py
@@ -183,6 +183,8 @@ def get_news_text(news_url):
         options = webdriver.ChromeOptions()
 
         options.add_argument("--headless=new")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
 
         driver = webdriver.Chrome(options=options)
 
