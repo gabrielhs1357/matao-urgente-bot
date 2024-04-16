6f25c2b0e309c43a62c4c788127ad4d0b28c4566
gabrielhs1357 <gabrielhs1357@gmail.com>
2024-04-16 00:21:45 -0300

    adjust code to run every 1 hour

--- src/main.py
+++ src/main.py
@@ -3,20 +3,15 @@ import sys
 
 from dotenv import load_dotenv
 
-logging.basicConfig(
-    level=logging.INFO,
-    handlers=[
-        logging.StreamHandler(sys.stdout)
-    ]
-)
+logging.basicConfig(level=logging.INFO, handlers=[logging.StreamHandler(sys.stdout)])
 
 load_dotenv()
 
-if __name__ == '__main__':
-    logging.info('Starting the application...')
+if __name__ == "__main__":
+    logging.info("Starting the application...")
 
-    import scripts.find_and_save_next_news as find_and_save_next_news
-    import scripts.find_and_tweet_next_news as find_and_tweet_next_news
+    import scripts.find_and_save_filtered_tweets as find_and_save_filtered_tweets
+    import scripts.tweet_last_news as tweet_last_news
 
-    find_and_save_next_news.run()
-    find_and_tweet_next_news.run()
+    find_and_save_filtered_tweets.run()
+    tweet_last_news.run()
